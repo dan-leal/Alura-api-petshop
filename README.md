8877bb9eb97d78823b7c0f63926d3ce0c341de58
Daniel Leal <daniel_leal25@gmail.com>
2021-07-07 12:51:58 -0400

    código sem node modules

---
+++ README.md
@@ -0,0 +1,5 @@
+https://cursos.alura.com.br/course/nodejs-api-rest-padronizada-escalavel# 
+
+Alura-api-petshop
+
+
---
+++ api/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const config = require('config')
+
+app.use(express.json())
+
+app.listen(config.get('api.porta'), ()=> console.log('A Api está funcionando!'))
