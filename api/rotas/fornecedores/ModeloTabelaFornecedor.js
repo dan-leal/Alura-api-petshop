bf60a25618c62c949a07700706a694d7b5b2d7f7
Daniel Leal <daniel_leal25@gmail.com>
2021-07-15 14:08:24 -0400

    escondendo dados sensíveis

--- api/rotas/fornecedores/ModeloTabelaFornecedor.js
+++ api/rotas/fornecedores/ModeloTabelaFornecedor.js
@@ -1,27 +1,27 @@
-const Sequelize = require('sequelize')
-const instancia = require('../../banco-de-dados')
+const Sequelize = require("sequelize");
+const instancia = require("../../banco-de-dados");
 
 const colunas = {
     empresa: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
     },
     email: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
     },
     categoria: {
-        type: Sequelize.ENUM('ração','brinquedos'),
-        allowNull: false
-    }
-}
+        type: Sequelize.ENUM("ração", "brinquedos"),
+        allowNull: false,
+    },
+};
 
 const opcoes = {
     freezeTableName: true,
-    tableName : 'fornecedores',
+    tableName: "fornecedores",
     timestamps: true,
-    createdAt: 'dataCriacao',
-    updatedAt: 'dataAtualizacao',
-    version: 'versao'
-}
-module.exports = instancia.define('fornecedor', colunas, opcoes)
+    createdAt: "dataCriacao",
+    updatedAt: "dataAtualizacao",
+    version: "versao",
+};
+module.exports = instancia.define("fornecedor", colunas, opcoes);
--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -1,37 +1,37 @@
-const Modelo = require('./ModeloTabelaFornecedor')
-const NaoEncontrado = require('../fornecedores/../../erros/NaoEncontrado')
+const Modelo = require("./ModeloTabelaFornecedor");
+const NaoEncontrado = require("../fornecedores/../../erros/NaoEncontrado");
 module.exports = {
-    listar () {
-        return Modelo.findAll()
+    listar() {
+        return Modelo.findAll({ raw: true });
     },
 
-    inserir (fornecedor) {
-        return Modelo.create(fornecedor)
+    inserir(fornecedor) {
+        return Modelo.create(fornecedor);
     },
 
-    async pegarPorId (id){
+    async pegarPorId(id) {
         const encontrado = await Modelo.findOne({
-            where:{
-                id: id
-            }
-        })
+            where: {
+                id: id,
+            },
+        });
 
         if (!encontrado) {
-            throw new NaoEncontrado()
+            throw new NaoEncontrado();
         }
 
-        return encontrado
+        return encontrado;
     },
 
-    atualizar (id, dadosParaAtualizar) {
+    atualizar(id, dadosParaAtualizar) {
         return Modelo.update(dadosParaAtualizar, {
-            where:{id: id}
-        })
+            where: { id: id },
+        });
     },
 
-    remover (id){
-       return Modelo.destroy({
-            where: {id: id}
-        })
-    }
-}
+    remover(id) {
+        return Modelo.destroy({
+            where: { id: id },
+        });
+    },
+};
