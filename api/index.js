c2f62bc4e0aa29a84bbaeac619623fb78ed27327
Daniel Leal <daniel_leal25@gmail.com>
2021-07-16 13:53:26 -0400

    finalização do curso e incluindo retornar dados xml

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const CampoInvalido = require("./erros/CampoInvalido");
 const DadosNaoFornecidos = require("./erros/DadosNaoFornecidos");
 const ValorNaoSuportado = require("./erros/ValorNaoSuportado");
 const FormatosAceitos = require("./Serializador").formatosAceitos;
+const SerializadorErro = require("./Serializador").SerializadorErro;
 
 app.use(express.json());
 
@@ -43,10 +44,10 @@ app.use((erro, req, res, proximo) => {
     if (erro instanceof ValorNaoSuportado) {
         status = 406;
     }
-
+    const serializador = new SerializadorErro(res.getHeader("Content-Type"));
     res.status(status);
     res.send(
-        JSON.stringify({
+        serializador.serializar({
             mensagem: erro.message,
             id: erro.idErro,
         })
