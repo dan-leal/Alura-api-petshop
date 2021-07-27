d551679fd5eb31800cf7638e881ec866c934ceee
Daniel Leal <daniel_leal25@gmail.com>
2021-07-27 15:14:28 -0400

    aula 03 inicio

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -75,7 +75,17 @@ roteador.delete("/:idFornecedor", async (req, res, proximo) => {
 
 const roteadorProdutos = require("./produtos");
 
-const verificarFornecedor = (req, res, proximo) => {};
+const verificarFornecedor = async (req, res, proximo) => {
+    try {
+        const id = req.params.idFornecedor;
+        const fornecedor = new Fornecedor({ id: id });
+        await fornecedor.carregar();
+        req.fornecedor = fornecedor;
+        proximo();
+    } catch (erro) {
+        proximo(erro);
+    }
+};
 
 roteador.use("/:idFornecedor/produtos", verificarFornecedor, roteadorProdutos);
 
--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -3,13 +3,13 @@ const Tabela = require("./TabelaProduto");
 const Produto = require("./Produto");
 
 roteador.get("/", async (req, res) => {
-    const produtos = await Tabela.listar(req.params.idFornecedor);
+    const produtos = await Tabela.listar(req.fornecedor.id);
     res.send(JSON.stringify(produtos));
 });
 
 roteador.post("/", async (req, res, proximo) => {
     try {
-        const idFornecedor = req.params.idFornecedor;
+        const idFornecedor = req.fornecedor.id;
         const corpo = req.body;
         const dados = Object.assign({}, corpo, { fornecedor: idFornecedor });
         const produto = new Produto(dados);
@@ -24,7 +24,7 @@ roteador.post("/", async (req, res, proximo) => {
 roteador.delete("/:id", async (req, res) => {
     const dados = {
         id: req.params.id,
-        fornecedor: req.params.idFornecedor,
+        fornecedor: req.fornecedor.id,
     };
 
     const produto = new Produto(dados);
